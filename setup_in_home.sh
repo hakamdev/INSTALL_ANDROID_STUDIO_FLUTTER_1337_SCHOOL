307acf195afa08dc080e56d2fd48688ca8c748cc
hakamdev <ehakam@student.1337.ma>
2022-09-23 16:52:56 +0100

    fix PATH vars in setup_in_home.sh

--- setup_in_home.sh
+++ setup_in_home.sh
@@ -46,13 +46,15 @@ ZSH=$(cat $HOME/.zshrc | grep ANDROID_SDK_HOME);
 
 if [ "$ZSH" = "" ]; then
     echo "2 Updating PATH in .zshrc";
-    echo >> $HOME/.zshrc;
-    echo "export JAVA_HOME=\"/Applications/Android Studio.app/Contents/jre/Contents/Home\"" >> $HOME/.zshrc;
-    echo "export ANDROID_SDK_HOME=\"\$HOME/Library/Android/sdk\"" >> $HOME/.zshrc;
-    echo "export PATH=\"\$PATH:\$HOME/goinfre/Flutter/flutter/bin\"" >> $HOME/.zshrc;
-    echo "export PATH=\"\$PATH:/Applications/Android Studio.app/Contents/jre/Contents/Home/bin\"" >> $HOME/.zshrc;
-    echo "export PATH=\"\$PATH:\$HOME/Library/Android/sdk/platform-tools\"" >> $HOME/.zshrc;
-    echo "export PATH=\"\$PATH:\$HOME/Library/Android/sdk/tools/bin\"" >> $HOME/.zshrc;
+    echo >> ~/.zshrc
+    echo "export JAVA_HOME=\"/Applications/Android Studio.app/Contents/jre/Contents/Home\"" >> ~/.zshrc
+    echo "export PATH=\"\$PATH:/Applications/Android Studio.app/Contents/jre/Contents/Home/bin\"" >> ~/.zshrc
+    # echo "export ANDROID_SDK_HOME=\"\$HOME/Library/Android/sdk\"" >> ~/.zshrc
+    echo "export PATH=\"\$PATH:\$HOME/goinfre/Flutter/flutter/bin\"" >> ~/.zshrc
+    echo "export PATH=\"\$PATH:\$HOME/Library/Android/sdk/platform-tools\"" >> ~/.zshrc
+    # echo "export PATH=\"\$PATH:\$HOME/Library/Android/sdk/tools/bin\"" >> ~/.zshrc
+    echo "export PATH=\"\$PATH:\$HOME/Library/Android/sdk/cmdline-tools/latest/bin\"" >> ~/.zshrc
+    echo
 fi
 
 #############################################################################
@@ -64,23 +66,37 @@ if [ $? -eq "1" ]; then
     open /Applications/Managed\ Software\ Center.app;
 fi
 
-# echo "Good, now to the manual stuff :("
-# echo
-# #############################################################################
-# echo "1- Open Android Studio if it wasn't opened automatically and follow the instructions to install SDK!"
-# echo "2- Now that the SDK is installed, go to Plugins tab on the left side, and Install Flutter Plugin."
-# echo "3- Now, click on the 3 dots, on the top-right corner of Android St. and choose SDK Manager"
-# echo "4- On the SDK manager, navigate to the SDK Tools in the middle, and check Android SDK Command-line Tools"
-# echo "5-2 If you need an emulator you can download an Image later when you want to create a new emulator, by selecting AVD manager"
-# echo "5-3 Now, Click OK to start installing the extra stuff you checked."
-# echo "Finally, open a terminal and run the command: flutter doctor"
-# echo "You'll see some error, don't worry, just need to accept to licenses, run: flutter doctor --android-licenses"
-# echo "and accept all licenses, by writing Y and hitting Enter each time."
-# echo "The errors concerning XCode, you can install cocoapods using: brew install cocoapods"
-# echo "If you don't have brew installed already: install it in goinfre using:\ncurl -fsSL https://raw.githubusercontent.com/hakamdev/42homebrew/master/install.sh | zsh"
-# echo "The other XCode error can only be solved with Updating XCode, which requires Admin Previliges :("
+COLOR='\033[0;32m'
+COLOR2='\033[0;31m'
+COLOR3='\033[0;33m'
+NC='\033[0m'
+clear
+echo "$COLOR2 		⛔️⛔️⛔️⛔️⛔️⛔️⛔️⛔️⛔️⛔️⛔️⛔️⛔️⛔️⛔️⛔️⛔️⛔️⛔️⛔️⛔️⛔️⛔️⛔️⛔️⛔️" $NC
+echo "$COLOR2 		⛔️                                                ⛔️" $NC
+echo "$COLOR2 		⛔️            GOOD. NOW THE MANUAL WORK           ⛔️" $NC
+echo "$COLOR2 		⛔️            FOLLOW THESE INSTRUCTIONS           ⛔️" $NC
+echo "$COLOR2 		⛔️             ⬇️ ⬇️ ⬇️ ⬇️ ⬇️ ⬇️ ⬇️ ⬇️ ⬇️ ⬇️ ⬇️ ⬇️            ⛔️" $NC
+echo "$COLOR2 		⛔️                                                ⛔️" $NC
+echo "$COLOR2 		⛔️⛔️⛔️⛔️⛔️⛔️⛔️⛔️⛔️⛔️⛔️⛔️⛔️⛔️⛔️⛔️⛔️⛔️⛔️⛔️⛔️⛔️⛔️⛔️⛔️⛔️" $NC
+echo "0️⃣ -$COLOR3 OPEN A NEW TERMINAL WINDOW TO KEEP THESE INSTRUCTIONS VISIBLE" $NC
+echo "0️⃣ -$COLOR3 IF YOU ACCIDENTALLY CLOSE THIS WINDOW RUN:$COLOR curl -fsSL https://raw.githubusercontent.com/hakamdev/INSTALL_ANDROID_STUDIO_FLUTTER_1337_SCHOOL/master/show_instr.sh | zsh" $NC
+echo "1️⃣ - Open Android Studio if it wasn't opened automatically and follow the instructions to install SDK!"
+echo "2️⃣ - Now that the SDK is installed, go to Plugins tab on the left side, and Install Flutter Plugin."
+echo "3️⃣ - Now, click on the 3 dots, on the top-right corner of Android St. and choose SDK Manager"
+echo "4️⃣ - On the SDK manager, navigate to the SDK Tools in the middle, and check Android SDK Command-line Tools"
+echo "5️⃣ - If you need an emulator you can download an Image later when you want to create a new emulator, by selecting AVD manager"
+echo "6️⃣ - Now, Click OK to start installing the extra stuff you checked."
+echo "7️⃣ - Finally, open a terminal and run the command:$COLOR flutter doctor" $NC
+echo "🔴- You'll see some error, don't worry, just need to accept to licenses, run:$COLOR flutter doctor --android-licenses" $NC
+echo "   and accept all licenses, by writing $COLOR'y'$NC and hitting Enter each time." $NC
+echo "🔴- The errors concerning XCode, you can install cocoapods using:$COLOR brew install cocoapods" $NC
+echo "🔴- If you don't have brew installed already: install it in goinfre using:\n$COLOR    curl -fsSL https://raw.githubusercontent.com/hakamdev/42homebrew/master/install.sh | zsh" $NC
+echo "⛔️- The XCode error can only be solved with Updating XCode. You can't do that. So, just ignore it." $NC
 
-# echo "Gongrats!"
-# echo "Now, create a new project by Selecting New Flutter Project or New Project if you do Native Android"
-# echo "if you are asked for flutter SDK Path, here it is $HOME/goinfre/Flutter/flutter"
-# echo "After the project is created, you may need to show it where SDK is, the first time, so go to File > Project Structure > and where it says <No SDK>, select Android API 31..."
+echo "✅- Greeeeeat!" $NC
+echo "🔵- Now, create a new project by Selecting $COLOR New Flutter Project $NC or $COLOR New Project $NC if you do Native Android" $NC
+echo "🔵- If you are asked for flutter SDK Path, here it is $COLOR $HOME/goinfre/Flutter/flutter" $NC
+echo "🔵- After the project is created, you may need to show it where SDK is, the first time, so go to $COLOR File > Project Structure > $NC and where it says $COLOR<No SDK>$NC, select Android API 31..." $NC
+echo "🔵- If you want to create an emulator, go to $COLOR Tools > AVD Manager > $NC and create a new one. You can download an image from there too." $NC
+echo "🔵- If you want to run the app on your phone, you need to enable USB Debugging on your phone, and connect it to your computer." $NC
+echo "🛑- If you prefer VSCode. Then I can't help you. Figure it out yourself. You probably need to install Flutter and Dart extensions and stuff..." $NC
